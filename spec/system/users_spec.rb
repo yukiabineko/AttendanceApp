37571c38fc8ee1877f055b87b1e0a5bef8aca56c
abineko2 <yuki1980426@gmail.com>
2020-10-19 08:49:02 +0900

    update

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -50,10 +50,16 @@ describe "users", type: :system do
       end
     end
   end
-   
- 
 
-  
+  describe "item delete",js:true  do
+     it "success" do
+       visit root_path
+       click_on "del#{@user.id}"
+       expect(accept_confirm).to eq "削除しますか？"
+       expect(page).to have_selector ".alert-success", text: "削除しました。"
+     end
+         
+  end
   
   
 end
