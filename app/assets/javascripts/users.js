94de45c7521be5d8f34a275874109f2278f2dd4f
yukiabineko <yukiabineko@live.com>
2020-10-21 17:18:45 +0900

    update

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener('turbolinks:load',function(){
+  document.querySelectorAll(".accordion").forEach(function(btn){
+    $(btn).on("click", function() {
+        $(this).next().slideToggle();
+    });
+  })   
+});
+
